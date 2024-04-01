75d765829c0b7751ace4b993a60c07fd992554bc
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-04-01 20:15:21 +0530

    Add proc_R method to RPALParser and test procR

--- rpal_interpreter/rpal_parser.py
+++ rpal_interpreter/rpal_parser.py
@@ -4,8 +4,28 @@ class RPALParser(Parser):
     def __init__(self,src):
         super().__init__(src)
         
+    def proc_R(self):
+        """Parse the R production rule."""
         
-    def __Rn(self):
+        self.proc_Rn()
+        if self.nextToken().__class__ == SemiColonToken:
+            # read the semicolon token and ignore it
+            self.read(SemiColonToken.instance(), ignore=True)
+            
+        # check if the next token is a @ 
+        token = self.nextToken()
+        while token != None and token.isValue("@"):
+            # read the @ token and ignore it
+            self.read(OperatorToken.fromValue("@"), ignore=True)
+            
+            # next token should be an identifier
+            token = self.nextToken()
+            self.read(IdentifierToken.fromValue(token.value))
+            
+            self.proc_R()
+            self.buildTree("@", 3)
+        
+    def proc_Rn(self):
         """Parse the Rn production rule.
 
         Raises:
@@ -16,16 +36,20 @@ class RPALParser(Parser):
         """
         token = self.nextToken()
         if token.__class__ == IdentifierToken:
+            # read the identifier token
             self.read(token)
             if token.value in ["true", "false", "nil", "dummy"]:
+                # build the tree with the token value
                 self.buildTree(token.value, 0)
             pass
         elif token.__class__ in [IntegerToken, StringToken]:
+            # read the integer or string token
             self.read(token)
         elif token.__class__ == LParenToken:
+            # read and ignore the ( token
             self.read(token, ignore=True)
         else:
-            raise Exception("Invalid token")
+            raise InvalidTokenException.fromToken(token)
 
     def parse(self):
             """Parses the source program and returns the Abstract Syntax Tree (AST).
@@ -33,5 +57,6 @@ class RPALParser(Parser):
             Returns:
                 The Abstract Syntax Tree (AST) of the source program.
             """
+            self.proc_R()
             return self.getAST()
     
