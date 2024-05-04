5b99bba71ce1054a32f28014b9c5089a5222ecf5
sandali <pldsandali@gmail.com>
2024-05-04 22:46:15 +0530

    Added the control, stack and the env to 1 file

--- rpal_interpreter/cse_components.py
+++ rpal_interpreter/cse_components.py
@@ -1,5 +1,43 @@
 from .symbol import *
 
+class Control:
+    
+    """The control of the CSE machine."""
+    
+    def __init__(self, controlStructs):
+        self.control = [EnvMarker(0)].insertControlStructs(controlStructs[0])  
+     
+    
+    def peekRightMost(self):
+        right_most = self.control[-1]
+        return right_most
+    
+    def removeRightMost(self):
+        right_most = self.control.pop(-1)
+        return right_most
+        
+    def insertControlStructs(self, controlStruct):
+        for i in controlStruct.controlStruct:
+            self.control.append(i)
+            
+
+class Stack:
+    
+    """
+    The stack to evaluate the CONTROL.
+    """    
+    
+    def __init__(self):
+        self.stack = [EnvMarker(0)]
+    
+    def popStack(self):
+        popElement = self.stack.pop(0)
+        return popElement
+    
+    def pushStack(self, value):
+        self.stack.append(value)
+        
+        
 class Environment:
     
     """
@@ -42,3 +80,5 @@ class Environment:
         else:
             return self.parent.lookUpEnv(name)
         
+            
+            
