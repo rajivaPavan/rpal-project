567525dc2cb481b1d592e00ad63ad1bfeda70b3a
sandali <pldsandali@gmail.com>
2024-05-04 16:14:33 +0530

    moved the control structs into the cse machine

--- rpal_interpreter/cse_control.py
+++ rpal_interpreter/cse_control.py
@@ -4,22 +4,9 @@ class Control:
     
     """The control of the CSE machine."""
     
-    def __init__(self, st):
-        
-        self.controlStructs: ControlStruct = self.generateControlStructs(st)    #an array which contains the control structures
-        self.control = [EnvMarker(0)].insertControlStructs(self.controlStructs[0])  #
-        
-        
-    def generateControlStructs(self, st) -> list:
-        """Calls the preorder traversal of the ST to generate the control structures."""	
-        self.traversePreOrder(st)
-        
-    def traversePreOrder(self, st):
-        """Traverses the ST in preorder to generate the control structures."""
-        # not implemented
-        if st.root == None:
-            return
-
+    def __init__(self, controlStructs):
+        self.control = [EnvMarker(0)].insertControlStructs(controlStructs[0])  
+     
     
     def peekRightMost(self):
         right_most = self.control[-1]
@@ -35,14 +22,3 @@ class Control:
             
             
             
-class ControlStruct:
-    
-    def __init__(self, index):
-        
-        """
-        Represents a control structure in the CSE machine.
-        eg: delta1, delta 0
-        """
-        
-        self.index = index
-        self.controlStruct = []
